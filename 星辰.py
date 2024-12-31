bb01b6d9d634042875d27f7c79de46ae6b2e2b3e
H-H <hsiaoiao@hotmail.com>
2024-12-31 14:58:50 +0800

    Delete 星辰.py

--- 星辰.py
+++
@@ -1,8 +0,0 @@
-{
-  "分类url":"https://citytv.cc/tv/dianying/area/{area}/by/{by}/class/{class}/id/{cateId}/page/{catePg}/year/{year}",
-  "分类": "电视剧$dianshiju#电影$dianying#动漫$dongman#综艺$zongyi#短剧$duanju",
-  "简介":"💫看海🌊祝朋友们观影愉快！💫+class=\"detail-sketch\">&&</span>",
-  "线路数组": "#playlist&&</li>",
-	"线路标题": "✨看海✨+>&&</a>[排序:神马云播>阿里云播>]",
-  "播放标题":">&&<[不包含:1080P]"
-  } 
